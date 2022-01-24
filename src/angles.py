b3cde6400a5912f1a5543dd195b51109fe7cf672
Stefano Silvestri <stefano.silvestri89@gmail.com>
2022-01-24 17:33:53 +0100

    modify class methods and environment dependencies

--- src/angles.py
+++ src/angles.py
@@ -26,74 +26,50 @@ class Angle:
         """
         calculates the orbit of the angle num/den under the doubling map.
 
-        Returns
-        -------
-        list
-            the elements in the orbit of the angle
+        Use the attribute `orbit_list` to get the result
         
         """
-        oList=[Frac(self.num,self.den)]
-        while len(set(oList)) == len(oList):
-            oList.append(oList[-1]*Frac(2,1)%1) #double it modulo 1
-        return oList
+        self.orbit_list=[Frac(self.num,self.den)]
+        while len(set(self.orbit_list)) == len(self.orbit_list):
+            self.orbit_list.append(self.orbit_list[-1]*Frac(2,1)%1) #double it modulo 1
     
-    def period(self,orbit=None):
+    def period(self):
         """
-        calculates the period of the angle num/den under the doubling map.
+        calculates the period of the angle num/den under the doubling map and 
+        the index in the orbit list of where it starts.
 
-        Arguments
-        ---------
-        orbit: list
-            the orbit of the angle
-
-        Returns
-        -------
-        list
-            the first entry is the index of where the period starts in the orbit 
-            the second entry is the length of the period
+        Use the attribute `start_index_per` to get the index.
+        Use the attribute `perLen` to get the length of the period.
         
         """
-        orb = self.orbit() if orbit is None else orbit
+        orb = self.orbit_list
         
-        initPer = orb.index(orb[-1]) #where the period starts
-        perLen = (len(orb)-initPer)-1 #length of the period in ks
-        return [initPer,perLen]
+        self.start_index_per = orb.index(orb[-1]) #where the period starts in the orbit
+        self.per_len = (len(orb)-self.start_index_per)-1 #length of the period in ks
 
 
-    def ks_from_angle(self,orbit=None,period=None):
+    def ks_from_angle(self):
         """
         finds the kneading sequence associated to the angle num/den.
-
-        Arguments
-        ---------
-        orbit: list
-            the orbit of the angle
-        period: list
-            the index indicating where the period of the orbit starts and the length of it
-
-
-        Returns
-        -------
-        str
-            a string consisting of 1s and 0s
+        
+        Use the attribute `ks` to get the result
         
         """
-        ks=''
-        orb = self.orbit() if orbit is None else orbit
-        perLen = self.period(orb)[1] if period is None else period[1]
+        self.ks=''
+        orb = self.orbit_list
         
         for numb in orb:
             if numb<Frac(self.num+self.den,2*self.den):
-                ks+='1'
+                self.ks+='1'
             else:
-                ks+='0'
+                self.ks+='0'
 
-        if perLen==1:
-            ks=ks[:-1]
-            ks+='0'
-        return ks
+        if self.per_len==1:
+            self.ks=self.ks[:-1]
+            self.ks+='0'
+
     
-    def attr_itin_from_ks(self,ks,orbit=None,period=None):
+    def attr_itin_from_ks(self):
         """
         finds the itinerary of 0 in the attractor given the kneading sequence 
         associated to the angle num/den.
@@ -101,57 +77,39 @@ class Angle:
         an entry of 1 in the ks corresponds to a change of sign in the itinerary,
         while an entry of 0 in the ks corresponds to the previous sign.
 
-        Parameters
-        ----------
-        ks: str
-            the kneading sequence
-
-        Arguments
-        ---------
-        orbit: list
-            the orbit of the angle
-        period: list
-            the index indicating where the period of the orbit starts and the length of it
-
-
-        Returns
-        -------
-        str
-            a string consisting of +s and -s
+        Use the attribute `itin` to get the result
         
         """
-        itin='+-' #we assume that ks starts with 1
+        self.itin='+-' #we assume that ks starts with 1
         lastSign='-' #the current last symbol in the itinerary
 
-        for i in range(1,len(ks)):
-            vi=ks[i]
+        for i in range(1,len(self.ks)):
+            vi=self.ks[i]
             if vi=='1':
                 if lastSign=='+':
-                    itin+='-'
+                    self.itin+='-'
                     lastSign='-'
                 else:
-                    itin+='+'
+                    self.itin+='+'
                     lastSign='+'
             else: #if vi=0
                 if lastSign=='+':
-                    itin+='+'
+                    self.itin+='+'
                     lastSign='+'
                 else:
-                    itin+='-'
+                    self.itin+='-'
                     lastSign='-'
         
-        orb = self.orbit() if orbit is None else orbit
-        initPer = self.period(orb)[0] if period is None else period[0]
-        perLen = self.period(orb)[1] if period is None else period[1]
-
-        if perLen==1: 
+        orb = self.orbit_list
+        
+        if self.per_len==1: 
             #if the period of ks is 1, then the itin is complete, so remove the last char
-            itin=itin[:-1]
+            self.itin=self.itin[:-1]
         else:
             #otherwise check if the period of the itin is twice the one of the ks
             temp=''
-            for i in range(initPer,len(ks)):
-                vi=ks[i]
+            for i in range(self.start_index_per,len(self.ks)):
+                vi=self.ks[i]
                 if vi=='1':
                     if lastSign=='+':
                         temp+='-'
@@ -167,106 +125,60 @@ class Angle:
                         temp+='-'
                         lastSign='-'
 
-            if temp!=itin[initPer+1:]:
-                itin+=temp
+            if temp!=self.itin[self.start_index_per+1:]:
+                self.itin+=temp
 
-        return itin
 
-    def period_length_itin(self,itin,period):
+    def period_length_itin(self):
         """
         finds the period length of the itinerary of 0 in the attractor.
-
-        Parameters
-        ----------
-        itin: str
-            the itinerary of 0
-
-        Arguments
-        ---------
-        period: list
-            the index indicating where the period of the orbit starts and the length of it
-
-        Returns
-        -------
-        int
-            the length of the period
-        
+        Use the attribute `perLenItin` to get the value.
         """
+        self.itin_per_len = self.per_len if self.per_len==1 else (len(self.itin)-self.start_index_per-1)
 
-        initPer = period[0]
-        perLen = period[1]
-        perLenItin = perLen if perLen==1 else (len(itin)-initPer-1)
 
-        return perLenItin
-
-    def itin_to_rat(self, itin, period, pow_symb = '**'):
+    def itin_to_rat(self, pow_symb = '**'):
         """
         finds the numerator of the rational function associated to the itinerary of 0.
-        
-        Parameters
-        ----------
-        itin: str
-            the itinerary of 0
+
+        Use the attribute `rat_func` to get the result
 
         Arguments
         ---------
-        period: list
-            the index indicating where the period of the orbit starts and the length of it
         pow_symb: str
             the symbol for exponentiation. Default is **. Use ^ if you need to use 
             the result in Mathematica.
 
-        Returns
-        -------
-        str
-            a string containing a polynomial in x.
-        
         """
 
-        perLenItin = self.period_length_itin(itin,period)
-        initPer = period[0]
+        self.rat_func = '('
         
-        rat='('
-        
-        for pos in range(len(itin)):
-            if ((perLenItin==1) & (pos==initPer)) :
-                rat+=')*(1-x) +('+itin[pos]+'x'+pow_symb+str(pos)+')'
-            elif pos==initPer+1:
-                rat+=')*(1-x'+pow_symb+str(perLenItin)+') +('+itin[pos]+'x'+pow_symb+str(pos)
-            elif pos==len(itin)-1:
-                rat+=itin[pos]+'x'+pow_symb+str(pos)+')'
+        for pos in range(len(self.itin)):
+            if ((self.itin_per_len==1) & (pos==self.start_index_per)) :
+                self.rat_func += ')*(1-x) +('+self.itin[pos]+'x'+pow_symb+str(pos)+')'
+            elif pos==self.start_index_per+1:
+                self.rat_func += ')*(1-x'+pow_symb+str(self.itin_per_len)+') +('+self.itin[pos]+'x'+pow_symb+str(pos)
+            elif pos==len(self.itin)-1:
+                self.rat_func += self.itin[pos]+'x'+pow_symb+str(pos)+')'
             else:
-                rat+=itin[pos]+'x'+pow_symb+str(pos)
+                self.rat_func += self.itin[pos]+'x'+pow_symb+str(pos)
         
-        return rat
 
-    def assoc_lambda(self,rat_func):
+    def assoc_lambda(self):
         """
         find the roots of the associated rational function inside the disc of radius 2^(-1/2)
-
-        Parameters
-        ----------
-        rat_func: str
-            the numerator of the rational function
         
-        Returns
-        -------
-        set
-            the set of roots
-
+        Use the attribute `lam` to get the result
         """
 
-        rat = rat_func
-        
         x = Symbol('x')
         f = Function('f')(x)
 
-        f = simplify(rat)
+        f = simplify(self.rat_func)
         allroots = solveset(f)
         la = Intersection(allroots,ConditionSet(x,( np.abs(x)<=(1/np.sqrt(2)) ), S.Complexes ))
         try:
-            lam = la.args[0]
+            self.lam = la.args[0]
             # raise Exception("possibly no roots")
         except:
-            return la
-        return lam
+            self.lam = la
